e44af50549929ccffe11c12aed57c2e17b3bfbb2
Francisco <f.alvarezcorrea1@gmail.com>
2024-01-09 14:26:10 -0300

    fix redireccion formulario

--- src/componentes/card.js
+++ src/componentes/card.js
@@ -1,13 +1,8 @@
 import "./card.css"
-import { useNavigate } from 'react-router-dom';
-import { useUser } from "./userLogin";
 
 function Card(props) {
 
   
-  const { user } = useUser();
-  
-  const navigate = useNavigate();
 
   function classes() {
     const bg = props.bgcolor ? " bg-" + props.bgcolor : " ";
@@ -18,17 +13,13 @@ function Card(props) {
     }
     return "card mb-3 card" + bg + txt;
   }
-  function handleClick() {
-    user ? navigate("/bank") : navigate("/createAccount");
-    
-  }
   return (
     <div className={classes()}>
       <div className="card-header">{props.header}</div>
       <div className="card-body">
         {props.title && <h5 className="card-title">{props.title}</h5>}
         {props.text && <p className="card-text">{props.text}</p>}
-        {props.body && <div className="body-card" onClick={handleClick}>{props.body}</div>}
+        {props.body && <div className="body-card">{props.body}</div>}
         {props.status && <div id="createStatus">{props.status}</div>}
       </div>
     </div>
